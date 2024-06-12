3e0b9304e9617cf3bcd14dabe9a1e08d19ddcb60
Abdullah Al Mamun <mamuneteruet@gmai.com>
2024-06-12 18:22:38 +0600

    try left section added

--- script.js
+++ script.js
@@ -31,6 +31,10 @@ arr.forEach(button => {
             pinIn.value = string;
         }
     })})
+//try left area var declaration
+    var tryLeft = document.getElementById('tryLeft')
+var numTryLeft = parseInt(tryLeft.innerText);
+
 
   const submitBtn = document.getElementsByClassName('submit-btn')[0]
   submitBtn.addEventListener('click',function()
@@ -41,11 +45,19 @@ arr.forEach(button => {
         document.getElementById('notify-fail').style.display = 'none';
     }
     else{
+        numTryLeft--;
+        //console.log(numTryLeft);
+        tryLeft.innerText = numTryLeft + ' try left';
+        if(numTryLeft == 0)
+            {
+                submitBtn.disabled = true;
+            }
         document.getElementById('notify-fail').style.display = 'block';
         document.getElementById('notify-success').style.display = 'none';
-        
+
     }
 })
+
 
 
 
