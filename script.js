a0b9f59a8322186c0d4ef90d2fc94c619f429870
Abdullah Al Mamun <mamuneteruet@gmai.com>
2024-06-12 17:44:22 +0600

    without 3 left

---
+++ script.js
@@ -0,0 +1,53 @@
+document.getElementById('notify-success').style.display = 'none';
+        document.getElementById('notify-fail').style.display = 'none';
+
+const generateBtn = document.getElementsByClassName('generate-btn')[0];
+var pinOut = document.getElementById('pinOut');
+
+generateBtn.addEventListener('click', function () {
+    const randomNum = Math.random() * 9000
+    const token = Math.floor(1000 + randomNum)
+    //console.log(token);
+    pinOut.value = token ;
+});
+
+let pinIn = document.getElementById('pinIn');
+let buttons = document.getElementsByClassName('button');
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click',(e) => { 
+        let value = e.target.innerHTML;
+        if(value == 'C'){
+            string = "";
+            pinIn.value = string;
+        }
+        else if(value == '&lt;'){
+            string = string.slice(0,-1);
+            pinIn.value = string;
+        }
+        else{
+            string += value;
+            pinIn.value = string;
+        }
+    })})
+
+  const submitBtn = document.getElementsByClassName('submit-btn')[0]
+  submitBtn.addEventListener('click',function()
+{
+    //console.log('hello')
+    if(pinOut.value == pinIn.value){
+        document.getElementById('notify-success').style.display = 'block';
+        document.getElementById('notify-fail').style.display = 'none';
+    }
+    else{
+        document.getElementById('notify-fail').style.display = 'block';
+        document.getElementById('notify-success').style.display = 'none';
+        
+    }
+})
+
+
+
+
+
